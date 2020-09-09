8b67f86eabef8931f5808a101e0d24f3d2f7624e
Markus Ånöstam <830794+Markus-ipse@users.noreply.github.com>
2020-09-09 21:27:29 +0200

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,71 @@
 import { files } from "./blend-files";
-import { formatDistance } from "date-fns";
+import { formatDistance, isAfter } from "date-fns";
+import util = require("util");
+import fs = require("fs");
+import ejs = require("ejs");
 
-const transformed = files.map((f) => {
+const mkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
+
+interface File {
+  fileName: string;
+  dir?: string;
+  created: string;
+  lastModified: string;
+}
+
+const transformFile = (f: File) => {
   const created = new Date(f.created);
   const lastModified = new Date(f.lastModified);
   const dateDiff = formatDistance(created, lastModified);
-  return { ...f, dateDiff };
+  const createdAfterModified = isAfter(created, lastModified);
+
+  return {
+    ...f,
+    dateDiff,
+    createdAfterModified: createdAfterModified ? "true" : "",
+  };
+};
+
+const transformed = files
+  .map(transformFile)
+  .sort((a, b) => a.lastModified.localeCompare(b.lastModified));
+
+const grouped: { [dir: string]: ReturnType<typeof transformFile>[] } = {};
+
+transformed.forEach((f) => {
+  if (f.dir) {
+    grouped[f.dir] = [...(grouped[f.dir] ?? []), f];
+  }
 });
 
-console.log(transformed);
+const columns = [
+  "fileName",
+  "dir",
+  "created",
+  "lastModified",
+  "dateDiff",
+  "createdAfterModified",
+];
+
+async function render() {
+  try {
+    //create output directory
+    await mkdir("dist", { recursive: true });
+
+    //render ejs template to html string
+    const html = await ejs
+      .renderFile(__dirname + "/index.ejs", {
+        columns,
+        projects: Object.values(grouped),
+      })
+      .then((output) => output);
+
+    //create file and write html
+    await writeFile("dist/index.html", html, "utf8");
+  } catch (error) {
+    console.log(error);
+  }
+}
+console.log(__dirname);
+render();
