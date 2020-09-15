650a201d731e28024f72cd0165764bfc9b64dc5a
Markus <830794+Markus-ipse@users.noreply.github.com>
2020-09-15 20:12:17 +0200

    graph improvements

--- src/get-modified-files.ts
+++ src/get-modified-files.ts
@@ -20,7 +20,7 @@ export async function* getFiles(dir: string): AsyncGenerator<string | Dirent> {
   }
 }
 
-export const getModfiedFilerPerMonth = async () => {
+export const getModfiedFilesPerMonth = async () => {
   const changedFilesPerMonth: { [key: string]: string[] } = {};
 
   for await (const f of getFiles(BLENDER_PROJECTS_PATH)) {
@@ -53,13 +53,13 @@ export const getModfiedFilerPerMonth = async () => {
 
     changedFilesPerMonth[key].push(f.substr(BLENDER_PROJECTS_PATH.length));
   }
-  // console.log(changedFilesPerMonth);
+  console.log(changedFilesPerMonth);
 
   return changedFilesPerMonth;
 };
 
 export const getLineData = async () => {
-  const modified = await getModfiedFilerPerMonth();
+  const modified = await getModfiedFilesPerMonth();
 
   const datesWithModifiedFiles = Object.keys(modified).sort((a, b) =>
     a.localeCompare(b)
@@ -67,10 +67,8 @@ export const getLineData = async () => {
 
   const allDates = createMonthSeries(datesWithModifiedFiles[0]);
 
-  console.log(datesWithModifiedFiles, allDates);
-
   const lineData = allDates.map((dateKey) => ({
-    x: dateKey,
+    x: getLabel(dateKey),
     y: modified[dateKey]?.length ?? 0,
   }));
 
@@ -78,12 +76,18 @@ export const getLineData = async () => {
 };
 
 function getKey(modified: Date) {
-  let month = "" + modified.getMonth();
+  let month = "" + (modified.getMonth() + 1);
 
   if (month.length === 1) month = "0" + month;
 
   const key = modified.getFullYear() + "-" + month;
   return key;
+}
+
+function getLabel(key: string) {
+  const [year, month] = key.split("-");
+
+  return (months as any)[month] + ", " + year;
 }
 
 function createMonthSeries(minDate: string) {
@@ -99,3 +103,18 @@ function createMonthSeries(minDate: string) {
 
   return series;
 }
+
+const months = {
+  "01": "Jan",
+  "02": "Feb",
+  "03": "Mar",
+  "04": "Apr",
+  "05": "May",
+  "06": "Jun",
+  "07": "Jul",
+  "08": "Aug",
+  "09": "Sep",
+  "10": "Oct",
+  "11": "Nov",
+  "12": "Dec",
+};
